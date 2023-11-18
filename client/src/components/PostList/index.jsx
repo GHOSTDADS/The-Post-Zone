631e323eabab40632244acd881090d427539f96f
Alexander Babic <alexander.t.b@hotmail.com>
2023-11-18 15:16:46 +0800

    added a postlist component and fixed user post query

---
+++ client/src/components/PostList/index.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client";
+import { QUERY_USER_POSTS } from '../../utils/queries';
+import Auth from '../../utils/auth';
+import { Navigate } from 'react-router-dom';
+import Posts from "../Posts";
+
+
+const PostList = () => {
+
+    const userId = Auth.getUser();
+    const { loading, data } = useQuery(QUERY_USER_POSTS, {variables: { userId: userId }});
+    const userPosts = data?.postsbyUser;
+
+    console.log("userPosts", userPosts);
+    const isLoggedIn = Auth.loggedIn();
+    if(!isLoggedIn){
+        return (
+            <Navigate to="/" replace={true} />
+        )
+    }
+
+
+
+    return (
+
+        <>
+        <div>Timeline Post</div>
+        {loading ? <div> lording </div> : <Posts userPosts={userPosts} />}
+
+        </>
+
+    );
+
+};
+
+export default PostList;
