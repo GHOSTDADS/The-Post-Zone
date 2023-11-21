c92cf266b8ca57a0e700a02bd8cce445274facc0
Alexander Babic <alexander.t.b@hotmail.com>
2023-11-21 19:37:52 +0800

    added path for assets

--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   
+  app.use('/images', express.static(path.join(__dirname, '../client/public/assets')));
+
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
